bf7aec4d836117aa183f1b5b81709290df1bb19c
Alexander Besuden <43247793+Abesuden@users.noreply.github.com>
2020-05-07 20:32:07 -0400

    Create main.rs

---
+++ Rust/main.rs
@@ -0,0 +1,18 @@
+mod printFun; // <-- imports module.
+mod variables; // <-----' |  |  |  |
+mod dataTypes; // <-------'  |  |  |
+mod stringy; // <------------'  |  :
+mod tuples; // <----------------'  ,
+mod arrays; // <-------- -    -   
+// mod vactorArrays; // <--  - - 
+
+
+fn main() { // <-- function called main
+    // printFun::run(); // from module, use the function run()
+    // variables::run(); // from module, use the function run()
+    // dataTypes::run(); // from module, use the function run()
+    // stringy::run(); // from module, use the function run()
+    // tuples::run(); // from module, use the function run()
+    arrays::run(); // from module, use the function run()
+    //vectorArrays::run(); // from module, use the function run()
+}
